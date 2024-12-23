86d4f48349c5d8381dbd65691a95d904829fe943
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-23 14:40:48 +0100

    fix 3

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
     const { email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      lerkId: id ?? 'default-id',
+      clerkId: id ?? 'default-id',
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
