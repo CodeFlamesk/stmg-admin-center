32321a93a9e1a435d956f42be1b8d49f2b655890
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-23 14:30:04 +0100

    id!

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
     const { email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      clerkId: id,  // Тепер clerkId гарантовано не undefined
+      clerkId: id!,  // Тепер clerkId гарантовано не undefined
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
