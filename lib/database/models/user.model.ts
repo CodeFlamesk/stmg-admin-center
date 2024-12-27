48928154db08d9f0872e8e75ad4d7c0b306b2184
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-27 14:36:23 +0100

    update models

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,7 @@
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerlId: { type: String, require: true, unique:true },
+    clerkId: { type: String, require: true, unique:true },
     email: { type: String, require: true, unique:true },
     username: { type: String, require: true, unique:true },
     firstName: {type: String, required: true },
