fb608048368f75135c54d3302ca0cae237133fcc
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-27 16:03:05 +0100

    fix required name

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,8 +4,10 @@ const UserSchema = new Schema({
     clerkId: { type: String, require: true, unique:true },
     email: { type: String, require: true, unique:true },
     username: { type: String, require: true, unique:true },
-    firstName: {type: String, required: true },
-})
+    firstName: {type: String },
+    lastName: {type: String},
+    photo: { type: String, required: true },
+    })
 const User = models.User || model('User', UserSchema);
 
 export default User;
