1a86d9e52d2e42fb0bcbaadb771a09e426e1c93d
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-28 11:50:41 +0100

    old code ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -11,6 +11,7 @@ export async function POST(req: Request) {
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
+  
 
   const headerPayload = await headers();
   const svix_id = headerPayload.get('svix-id');
