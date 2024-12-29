c3f3d1d50d0e36e78d8ae7808896587e9fb334df
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-12-29 18:33:08 +0100

    install react

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
   styles: string;
 }
 
-const Container: React.FC<ContainerProps> = ({ children,styles }) => {
+const Container: React.FC<ContainerProps> = ({ children, styles }) => {
   return (
     <div className={`w-full max-w-[1470px]  mx-auto px-[15px] bg-white ${styles} `}>
       {children}
